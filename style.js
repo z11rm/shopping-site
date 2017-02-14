196540f882a505d0438facf18bcee76e35972f4f
FukumiKoshikawa <ee37064@meiji.ac.jp>
2017-02-14 04:51:23 +0000

    repair now

--- style.js
+++ style.js
@@ -1,3 +1,14 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel(
+        {
+            loop: true,
+            nav: false,
+            dots: true,
+            items: 1,
+            autoplay: true
+        }
+    );
+});
 //oritatami---------------
 
 $(function () {
